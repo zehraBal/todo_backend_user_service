9d031f5fc531f9255472c1b7d1c41938b242e040
zehraBal <fz.bal00@gmail.com>
2025-07-22 00:49:28 +0300

    feat: add UserResponseDTO for safe user data exposure

---
+++ src/main/java/com/bal/user_service/dto/UserResponseDTO.java
@@ -0,0 +1,14 @@
+package com.bal.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO {
+    private String id;
+    private String username;
+    private String email;
+}
