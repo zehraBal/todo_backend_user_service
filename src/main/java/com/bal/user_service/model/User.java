772deb13ebdf84a7bccd21a45bf82711798b219b
zehraBal <fz.bal00@gmail.com>
2025-07-21 21:27:08 +0300

    feat: create base User model for MongoDB

---
+++ src/main/java/com/bal/user_service/model/User.java
@@ -0,0 +1,37 @@
+package com.bal.user_service.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "users")
+// @Document --> Specifies that a Java class will map to a collection in MongoDB
+// A class that receives this annotation represents an entity to be stored in MongoDB
+// if collection parameter is not specified, Spring uses the class name in lowercase(User-->user)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String username;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private List<String> roles;
+
+}
