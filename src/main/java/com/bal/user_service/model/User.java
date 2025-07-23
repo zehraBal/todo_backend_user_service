49e5f06eed2d909faf066502eec7f4c7e02c3e31
zehraBal <fz.bal00@gmail.com>
2025-07-24 00:20:28 +0300

    chore(user): add @Builder annotation to User entity

--- src/main/java/com/bal/user_service/model/User.java
+++ src/main/java/com/bal/user_service/model/User.java
@@ -3,6 +3,7 @@ package com.bal.user_service.model;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -15,6 +16,7 @@ import java.util.List;
 // A class that receives this annotation represents an entity to be stored in MongoDB
 // if collection parameter is not specified, Spring uses the class name in lowercase(User-->user)
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
