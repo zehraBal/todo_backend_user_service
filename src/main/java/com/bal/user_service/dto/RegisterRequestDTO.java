3d97379a6d47641739f236c492d4f1ec7de47483
zehraBal <fz.bal00@gmail.com>
2025-07-24 00:20:13 +0300

    refactor(auth): rename LoginRequestDto to RegisterRequestDto

--- src/main/java/com/bal/user_service/dto/RegisterRequestDTO.java
+++ src/main/java/com/bal/user_service/dto/RegisterRequestDTO.java
@@ -1,13 +1,21 @@
 package com.bal.user_service.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginRequestDTO {
+public class RegisterRequestDTO {
+
     private String username;
+
+    private String email;
+
     private String password;
+
+    private List<String> roles;
 }
