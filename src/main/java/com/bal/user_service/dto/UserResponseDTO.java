104069bac101cb133ee406ce13f450dd393d6738
zehraBal <fz.bal00@gmail.com>
2025-08-03 23:51:16 +0300

    fix(user): change ID type from String to Long
    - Update `User.id` to Long (for PostgreSQL compatibility)
    - Fix `UserResponseDTO` to reflect Long ID
    - Adjust service layer to handle Long IDs

--- src/main/java/com/bal/user_service/dto/UserResponseDTO.java
+++ src/main/java/com/bal/user_service/dto/UserResponseDTO.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponseDTO {
-    private String id;
+    private Long id;
     private String username;
     private String email;
 }
